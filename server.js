cbcf311b9a3f5ed15f832d653d3e5e66978600b2
vivek <mehra.viv@gmail.com>
2020-11-18 12:52:24 +0530

    authors read and search

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRoute = require('./routes/index')
+const authorRoute = require('./routes/authors')
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
@@ -19,5 +22,6 @@ const db = mongoose.connection
 db.on('error',error => console.error(error))
 db.once('open',() => console.log('Connected to Mongoose'))
 app.use('/', indexRoute)
+app.use('/authors', authorRoute)
 
 app.listen(process.env.PORT || 3000)
